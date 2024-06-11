a26129a13096bd45429578fecfa7f51eb0bfcb4c
5m1l33y <joseandresgavilanes2012@gmail.com>
2024-06-11 01:19:40 +0100

    elpepe

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -6,11 +6,6 @@ import (
 	"os"
 )
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
 	fmt.Fprint(os.Stdout, "$ ")
-	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
 }
