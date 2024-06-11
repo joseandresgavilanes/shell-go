77165bac3f96e37e9f881b6165c92ef9c55fe2fa
5m1l33y <joseandresgavilanes2012@gmail.com>
2024-06-11 13:04:00 +0100

    creating the erro handling

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,8 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 func main() {
+	
 	fmt.Fprint(os.Stdout, "$ ")
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	
+	command, _:= bufio.NewReader(os.Stdin).ReadString('\n')
+	fmt.Printf("%s: command not found ", strings.TrimRight(command, "\n"))
 }
