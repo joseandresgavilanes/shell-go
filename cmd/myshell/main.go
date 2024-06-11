b1ce493687fcfd367f627b9854dd1fefdc62dfa2
5m1l33y <joseandresgavilanes2012@gmail.com>
2024-06-11 22:58:24 +0100

    creating the erro handling

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,24 +4,29 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
-
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
 	for {
-		cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		fmt.Fprint(os.Stdout, "$ ")
+		message, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
-			fmt.Println(err)
 			os.Exit(1)
 		}
-		cmd = strings.TrimSpace(cmd)
-		switch cmd {
-		case "exit 0":
-			os.Exit(0)
-
+		message = strings.TrimSpace(message)
+		commands := strings.Split(message, " ")
+		switch commands[0] {
+		case "exit":
+			code, err := strconv.Atoi(commands[1])
+			if err != nil {
+				os.Exit(1)
+			}
+			os.Exit(code)
 		case "echo":
-			fmt.Println(cmd[5:])
+			fmt.Fprintf(os.Stdout, "%s\n", strings.Join(commands[1:], " "))
+		default:
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", message)
 		}
 	}
 }
